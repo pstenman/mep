f0aa2d1b378ef6189bbf9aee027fa149d53d05e6
PETER STENMAN <PETER.STENMAN@hotmail.se>
2025-12-17 15:10:48 +0100

    fix (plan): add plans to router.

--- apps/api/src/trpc/router.ts
+++ apps/api/src/trpc/router.ts
@@ -2,10 +2,12 @@ import { stripeRouter } from "@/routers/stripe";
 import { createTRPCRouter } from "./server";
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 import { authRouter } from "@/routers/auth";
+import { planRouter } from "@/routers/plans";
 
 export const appRouter = createTRPCRouter({
   stripe: stripeRouter,
   auth: authRouter,
+  plans: planRouter,
 });
 
 export type AppRouter = typeof appRouter;
